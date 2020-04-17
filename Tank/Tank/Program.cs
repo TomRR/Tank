f78f954f2a1bba73b91fbe54cad40594d782559e
TomRR <resingtr@gmail.com>
2020-04-17 16:19:50 +0200

    init projekt and add Tank object

---
+++ Tank/Tank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
