34b551a8df30f4b72617906ad66460b052bf7b21
TomRR <resingtr@gmail.com>
2020-04-17 16:31:58 +0200

    add functions istBefuellenMoeglich and istEntnehmenMoeglich

--- Tank/Tank/Program.cs
+++ Tank/Tank/Program.cs
@@ -6,7 +6,54 @@ namespace Tank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Wichtige Hinweise:
+             * 
+             * - Der Fuellstand eines Tanks kann nur ueber die 
+             *   entsprechenden Methoden geaendert werden.
+             *   
+             * - Der Inhalt eines Tanks darf sich nur aendern,
+             *   wenn beim Befuellen die Menge noch hineinpasst,
+             *   bzw. beim Entnehmen noch verfuegbar ist.
+             */
+
+            int menge;
+            char auswahl = 'x';
+
+            Tank einTank = new Tank(1000, 200);
+
+            Console.WriteLine("Tankvolumen: {0} Liter", einTank.Volumen);
+            Console.WriteLine("Tankinhalt: {0} Liter", einTank.Fuellmenge);
+
+            do
+            {
+                Console.Write("\nWas moechten Sie tun: \n\tBefuellen (b)\n\tEntnehmen (e) \n\tBeenden (x)\n\t-->");
+                auswahl = Convert.ToChar(Console.ReadLine());
+
+                switch (auswahl)
+                {
+                    case 'b':
+                        Console.Write("Wieviel Liter moechten Sie einfuellen: ");
+                        menge = Convert.ToInt32(Console.ReadLine());
+
+                        if (einTank.istBefuellenMoeglich(menge))
+                            Console.WriteLine("Fuellmenge Tank: {0}", einTank.Fuellmenge );
+                        else
+                            Console.WriteLine("Es passen nur noch {0} Liter hinein", einTank.Volumen - einTank.Fuellmenge);
+                        break;
+
+                    case 'e':
+                        Console.Write("Wieviel Liter moechten Sie ennehmen: ");
+                        menge = Convert.ToInt32(Console.ReadLine());
+
+                        if (einTank.istEntnehmenMoeglich(menge))
+                            Console.WriteLine("Fuellmenge Tank: " + einTank.Fuellmenge);
+                        else
+                            Console.WriteLine("Es sind nur noch {0} Liter drin!", einTank.Fuellmenge);
+                        break;
+                }
+            }
+            while (auswahl != 'x');
         }
     }
 }
--- Tank/Tank/Tank.cs
+++ Tank/Tank/Tank.cs
@@ -6,5 +6,29 @@ namespace Tank
 {
     class Tank
     {
+        public Tank(int volumen, int fuellMenge)
+        {
+            Volumen = volumen;
+            Fuellmenge = fuellMenge;
+        }
+
+        public int Volumen { get; set; }
+        public int Fuellmenge { get; set; }
+
+        public bool istBefuellenMoeglich(int menge)
+        {
+            return (Volumen >= Fuellmenge + menge) ? true : false;
+        }
+        public bool istEntnehmenMoeglich(int menge)
+        {
+            return (Fuellmenge >= menge) ? true : false;
+        }
+
+        public int Befuellen(int menge)
+        {
+            return Fuellmenge + menge;
+        }
+
+
     }
 }
